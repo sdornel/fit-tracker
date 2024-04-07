74982573ccc77601c410feea83dce81b84aa3606
sdornel <kai_dornel@hotmail.com>
2024-04-07 14:34:36 -0400

    exercise files

--- backend-fit-tracker/src/app.module.ts
+++ backend-fit-tracker/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExerciseService } from './exercise/exercise.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ExerciseService],
 })
 export class AppModule {}
