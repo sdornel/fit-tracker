187c7a25d3f201575a8c28513194c7589dab1efe
sdornel <kai_dornel@hotmail.com>
2024-04-08 19:29:13 -0400

    update import path

--- backend-fit-tracker/src/user/user.repository.ts
+++ backend-fit-tracker/src/user/user.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/models/user';
+import { User } from 'src/entity/user.entity';
 import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
