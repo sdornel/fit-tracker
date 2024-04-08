666bbf29fa54674429f5f7702088c8287c76445e
sdornel <kai_dornel@hotmail.com>
2024-04-07 21:49:41 -0400

    typeorm

--- backend-fit-tracker/src/app.module.ts
+++ backend-fit-tracker/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExerciseService } from './exercise/exercise.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'postgres',
+      entities: [],
+      synchronize: false,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService, ExerciseService],
 })
--- backend-fit-tracker/src/exercise/exercise.service.ts
+++ backend-fit-tracker/src/exercise/exercise.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ExerciseService {
+  constructor(
+    // @InjectRepository(User)
+    // private userRepository: Repository<User>,
+  ) {}
   getExercises(): string {
     return 'Hello World!';
   }
