45eefb4357b4109349ea7d3cf1545e4c1631d768
sdornel <kai_dornel@hotmail.com>
2024-04-08 21:49:32 -0400

    prepare for exercise repository

--- backend-fit-tracker/src/exercise/exercise.service.ts
+++ backend-fit-tracker/src/exercise/exercise.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Exercise } from 'src/entities/exercise.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ExerciseService {
   constructor(
-    // @InjectRepository(User)
-    // private userRepository: Repository<User>,
+    @InjectRepository(Exercise)
+    private exerciseRepository: Repository<Exercise>,
   ) {}
   getExercises(): string {
     return 'Hello World!';
