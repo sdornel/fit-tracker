deb5ccc53ebfa5c0dcc6d4a2d43945b6a149a1f4
sdornel <kai_dornel@hotmail.com>
2024-04-08 21:48:27 -0400

    controller instead of resolver

---
+++ backend-fit-tracker/src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  async findOneById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOneById(id);
+  }
+}
