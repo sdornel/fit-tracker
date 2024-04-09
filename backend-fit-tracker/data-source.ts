f17de1ce89c1eee3d47d9e12d0cf801dde837aca
sdornel <kai_dornel@hotmail.com>
2024-04-08 21:42:18 -0400

    data source and config setup

---
+++ backend-fit-tracker/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '',
+    database: 'fit_tracker',
+    migrations: ["dist/migrations/*.js"],
+}
+const dataSource = new DataSource(dataSourceOptions)
+export default dataSource
