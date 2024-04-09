1ace9ac82de12a659aa768ee957fa93f23bcf8b4
sdornel <kai_dornel@hotmail.com>
2024-04-08 21:43:02 -0400

    remove console log

--- backend-fit-tracker/src/main.ts
+++ backend-fit-tracker/src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 require('dotenv').config();
 
-console.log(process.env.DB_TYPE); // This should print "postgres" if your .env is set
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
