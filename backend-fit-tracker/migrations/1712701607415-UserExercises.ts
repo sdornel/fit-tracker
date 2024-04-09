f07cd6350aa73767fa07f0f6b89ce3b9c5c7fe6c
sdornel <kai_dornel@hotmail.com>
2024-04-09 18:58:38 -0400

    new migration

---
+++ backend-fit-tracker/migrations/1712701607415-UserExercises.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+
+export class UserExercises1712701607415 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'userexercise',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'userId',
+                    type: 'int',
+                },
+                {
+                    name: 'exerciseId',
+                    type: 'int',
+                }
+            ],
+        }), true);
+
+        await queryRunner.createForeignKey('userexercise', new TableForeignKey({
+            columnNames: ['userId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'user',
+            onDelete: 'CASCADE'
+        }));
+
+        await queryRunner.createForeignKey('userexercise', new TableForeignKey({
+            columnNames: ['exerciseId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'exercise',
+            onDelete: 'CASCADE'
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // when rolling back, drop foreign keys and then the table itself
+        const userExerciseTable = await queryRunner.getTable('userexercise');
+
+        const foreignKeys = userExerciseTable.foreignKeys.filter(fk => fk.columnNames.indexOf('userId') !== -1 || fk.columnNames.indexOf('exerciseId') !== -1);
+        for (const fk of foreignKeys) {
+            await queryRunner.dropForeignKey('userexercise', fk);
+        }
+
+        await queryRunner.dropTable('userexercise');
+    }
+}
