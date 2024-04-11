a2456a8a7418dc230c04d4ff734cd4b0759784f1
sdornel <kai_dornel@hotmail.com>
2024-04-10 23:57:50 -0400

    install new package and update data source

--- backend-fit-tracker/src/db/data-source.ts
+++ backend-fit-tracker/src/db/data-source.ts
@@ -1,11 +1,11 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '',
-    database: 'fit_tracker',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     migrations: ['dist/db/migrations/*.js'],
 }
 const dataSource = new DataSource(dataSourceOptions)
