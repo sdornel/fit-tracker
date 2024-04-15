e16a00968472f2c41c00e4ef01813ae44e5e3ee7
sdornel <kai_dornel@hotmail.com>
2024-04-14 22:30:59 -0400

    comment for clarity

--- backend-fit-tracker/src/auth/local.strategy.ts
+++ backend-fit-tracker/src/auth/local.strategy.ts
@@ -6,8 +6,8 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-      super({
-        usernameField: 'email',
+    super({
+        usernameField: 'email', // by default passport wants to use username but you want to use email
         passwordField: 'password',
       });
   }
