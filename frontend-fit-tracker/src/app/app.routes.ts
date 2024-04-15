b1c14e85fba6ac73fc9d81f7eb68e05e526e71d8
sdornel <kai_dornel@hotmail.com>
2024-04-14 22:33:15 -0400

    add reminder

--- frontend-fit-tracker/src/app/app.routes.ts
+++ frontend-fit-tracker/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
-    { path: 'user', component: UserProfileComponent },
+    { path: 'user', component: UserProfileComponent }, // this needs to map to an individual user
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
