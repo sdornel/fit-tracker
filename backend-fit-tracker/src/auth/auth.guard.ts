ec974931d9a04e8fe9c112d14e982f84573a7d94
sdornel <kai_dornel@hotmail.com>
2024-04-15 22:31:07 -0400

    cleanup

--- backend-fit-tracker/src/auth/auth.guard.ts
+++ backend-fit-tracker/src/auth/auth.guard.ts
@@ -12,7 +12,6 @@ export class AuthGuard implements CanActivate {
   constructor(private readonly authService: AuthService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
     try {
       const request = context.switchToHttp().getRequest();
 
