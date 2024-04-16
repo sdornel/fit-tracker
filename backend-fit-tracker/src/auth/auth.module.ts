301e48c6850b2b87210a67deb2846f8f0bd0ed65
sdornel <kai_dornel@hotmail.com>
2024-04-15 22:31:20 -0400

    remove unused import

--- backend-fit-tracker/src/auth/auth.module.ts
+++ backend-fit-tracker/src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
