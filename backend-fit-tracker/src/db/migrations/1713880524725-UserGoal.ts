166f679fb7485e6019b69a9cf5b5b6ac2e560b2e
sdornel <kai_dornel@hotmail.com>
2024-04-23 09:56:58 -0400

    new migrations

---
+++ backend-fit-tracker/src/db/migrations/1713880524725-UserGoal.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserGoal1713880524725 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
