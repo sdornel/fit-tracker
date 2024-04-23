29928236fb02129e69d5c64d12e673e9e0e64dc3
sdornel <kai_dornel@hotmail.com>
2024-04-23 16:41:42 -0400

    detail modal has info

--- frontend-fit-tracker/src/app/user-profile/goals/goal-detail-modal/goal-detail-modal.component.ts
+++ frontend-fit-tracker/src/app/user-profile/goals/goal-detail-modal/goal-detail-modal.component.ts
@@ -1,14 +1,25 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Goal } from '../../../models/goal';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-goal-detail-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+  ],
   templateUrl: './goal-detail-modal.component.html',
   styleUrl: './goal-detail-modal.component.css'
 })
 export class GoalDetailModalComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public goal: Goal) {console.log('goal', goal)}
+  constructor(
+    public dialogRef: MatDialogRef<GoalDetailModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public goal: Goal
+    ) {}
+
+    closeDialog(): void {
+      this.dialogRef.close();
+    }
 }
