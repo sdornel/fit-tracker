47c0d2c32b2efe951147baf17896dd6b4fc57363
sdornel <kai_dornel@hotmail.com>
2024-04-23 10:08:24 -0400

    form can open

--- frontend-fit-tracker/src/app/user-profile/goals/edit-goal/edit-goal.component.ts
+++ frontend-fit-tracker/src/app/user-profile/goals/edit-goal/edit-goal.component.ts
@@ -1,19 +1,44 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Goal } from '../../../models/goal';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-edit-goal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatDialogModule, MatFormFieldModule],
   templateUrl: './edit-goal.component.html',
   styleUrl: './edit-goal.component.css'
 })
-export class EditGoalComponent {
-
+export class EditGoalComponent implements OnInit {
+  goalForm!: FormGroup;
   constructor(
+    private dialogRef: MatDialogRef<EditGoalComponent>,
     @Inject(MAT_DIALOG_DATA) public goal: Goal
-  ) {
+  ) {}
 
+  ngOnInit(): void {
+    console.log('this.goal', this.goal);
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.goalForm = new FormGroup({
+      title: new FormControl(this.goal.title, [Validators.required]),
+      notes: new FormControl(this.goal.notes, [Validators.required]),
+      deadline: new FormControl(this.goal.deadline, [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    const updatedGoal = {
+      ...this.goal,
+      ...this.goalForm.value
+    }
+    console.log('updatedGoal', updatedGoal);
+    if (this.dialogRef) {
+      this.dialogRef.close(updatedGoal);
+    }
   }
 }
