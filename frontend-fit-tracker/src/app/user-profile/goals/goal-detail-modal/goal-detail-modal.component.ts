75df17709cf30d525bc6372575f43b8c5bd48a0a
sdornel <kai_dornel@hotmail.com>
2024-04-25 12:38:09 -0400

    date pipe

--- frontend-fit-tracker/src/app/user-profile/goals/goal-detail-modal/goal-detail-modal.component.ts
+++ frontend-fit-tracker/src/app/user-profile/goals/goal-detail-modal/goal-detail-modal.component.ts
@@ -2,13 +2,21 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Goal } from '../../../models/goal';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule, DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 
 @Component({
   selector: 'app-goal-detail-modal',
   standalone: true,
   imports: [
+    CommonModule,
     MatDialogModule,
     MatButtonModule,
+  ],
+  providers: [
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: "longDate" }
+    }
   ],
   templateUrl: './goal-detail-modal.component.html',
   styleUrl: './goal-detail-modal.component.css'
