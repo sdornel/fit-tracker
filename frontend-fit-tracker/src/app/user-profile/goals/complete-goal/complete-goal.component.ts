070c62d874e80b3f7c1829d3acee47c40b489b59
sdornel <kai_dornel@hotmail.com>
2024-04-26 15:50:55 -0400

    complete goal modal

--- frontend-fit-tracker/src/app/user-profile/goals/complete-goal/complete-goal.component.ts
+++ frontend-fit-tracker/src/app/user-profile/goals/complete-goal/complete-goal.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-complete-goal',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatButtonModule
+  ],
   templateUrl: './complete-goal.component.html',
   styleUrl: './complete-goal.component.css'
 })
