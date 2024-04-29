740cb66ba236c4edbb41d417b07f610af8e33b96
sdornel <kai_dornel@hotmail.com>
2024-04-29 19:29:25 -0400

    create form

--- frontend-fit-tracker/src/app/user-profile/goals/create-goal/create-goal.component.ts
+++ frontend-fit-tracker/src/app/user-profile/goals/create-goal/create-goal.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-goal',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatDialogModule,
+  ],
   templateUrl: './create-goal.component.html',
   styleUrl: './create-goal.component.css'
 })
+// I should consider consolidating this with the edit form
 export class CreateGoalComponent {
+  goalForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.goalForm = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      notes: new FormControl(null, [Validators.required]),
+      deadline: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  onSubmit() {}
 }
