62752ff818ca62528871d454e5ca2cc0bdaa57a1
sdornel <kai_dornel@hotmail.com>
2024-04-29 20:41:25 -0400

    create goal client updates

--- frontend-fit-tracker/src/app/user-profile/goals/create-goal/create-goal.component.ts
+++ frontend-fit-tracker/src/app/user-profile/goals/create-goal/create-goal.component.ts
@@ -1,16 +1,25 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-create-goal',
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
     NgIf,
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './create-goal.component.html',
   styleUrl: './create-goal.component.css'
 })
